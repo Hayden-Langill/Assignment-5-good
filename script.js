3b5e9200ac5ea331f343595490a3a3ef4e35dcea
HaydenLangill <84516925+Hayden-Langill@users.noreply.github.com>
2021-06-15 14:11:44 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let d = document.getElementById('input').value
+const pi = Math.PI
+let n = 0
+
+document.getElementById('button').addEventListener('click', area)
+document.getElementById('button2').addEventListener('click', cir)
+
+function area () {
+  d = document.getElementById('input').value
+  d = parseInt(d)
+  r = d / 2
+  r = r * r
+  n = pi * r 
+  document.getElementById('answer').innerHTML = Math.floor(n)
+}
+
+function cir () {
+  d = document.getElementById('input').value
+  d = parseInt(d)
+  r = d / 2
+  n = (pi * r) * 2
+  document.getElementById('answer').innerHTML = Math.floor(n)
+}
